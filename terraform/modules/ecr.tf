bb87ebc51c8eee4a487cb06edd9a396d02baefc1
Jeremy Rako <jeremy.rako@couger.co.jp>
2025-03-25 14:53:52 +0900

    Add test lambda

--- terraform/modules/ecr.tf
+++ terraform/modules/ecr.tf
@@ -103,7 +103,7 @@ data "aws_iam_policy_document" "lambda_assume_role" {
 resource "aws_lambda_function" "get-managers-lambda-function" {
   function_name = "${var.app}-get-managers-lambda"
   timeout       = 5 # seconds
-  image_uri     = "${data.aws_ecr_repository.get-managers-lambda.repository_url}:latest"
+  image_uri     = "${aws_ecr_repository.get-managers-lambda.repository_url}:latest"
   package_type  = "Image"
 
   role = aws_iam_role.get-managers-lambda-function-role.arn
